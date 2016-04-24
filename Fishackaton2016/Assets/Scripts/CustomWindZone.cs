f835027731d9433c346d826b4a262165fb122091
Jeroen van Dragt <jeroen.van.dragt@gmail.com>
2016-04-24 11:27:59 +0200

    CustomWindZone

---
+++ Fishackaton2016/Assets/Scripts/CustomWindZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CustomWindZone : MonoBehaviour
+{
+    public List<Rigidbody> rbList = new List<Rigidbody>();
+
+    public void OnTriggerEnter ( Collider other )
+    {
+        if(other.GetComponent<Rigidbody>())
+        {
+            other.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1),ForceMode.Force) ;
+            if(!rbList.Contains(other.GetComponent<Rigidbody>()))
+                rbList.Add(other.GetComponent<Rigidbody>());
+        }
+    }
+
+    public void OnTriggerExit ( Collider other )
+    {
+        if(other.GetComponent<Rigidbody>())
+        {
+            if(rbList.Contains(other.GetComponent<Rigidbody>()))
+                rbList.Remove(other.GetComponent<Rigidbody>());
+        }
+    }
+}
